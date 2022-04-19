a2ccae5fb9c090ede70f666963e10300608475be
Gonzalo <100429091@alumnos.uc3m.es>
2022-04-19 17:28:15 +0200

    un poco de desplazarse y footer fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatInputModule } from '@angular/material/input';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -31,7 +35,13 @@ import { DesplazarsePageComponent } from './desplazarse-page/desplazarse-page.co
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/desplazarse-page/desplazarse-page.component.ts
+++ src/app/desplazarse-page/desplazarse-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-desplazarse-page',
   templateUrl: './desplazarse-page.component.html',
-  styleUrls: ['./desplazarse-page.component.scss']
+  styleUrls: ['./desplazarse-page.component.scss'],
 })
-export class DesplazarsePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DesplazarsePageComponent {
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      origin: ['', Validators.required],
+      destination: ['', Validators.required],
+    });
+  }
+  nada() {
+    console.log('nada');
   }
 
 }
